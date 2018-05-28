d3f63c9c4e3bd23e9ce829a495dc7b8617fb8e7c
Raphael Zanarelli <raphael.zanarelli@s2it.com.br>
2018-05-28 16:27:35 -0300

    projeto criado no spring initializr (web + lombok + postgresql + jpa)

---
+++ projeto1incubadora/src/main/java/br/com/s2it/incubadora/projeto1incubadora/Projeto1incubadoraApplication.java
@@ -0,0 +1,12 @@
+package br.com.s2it.incubadora.projeto1incubadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto1incubadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto1incubadoraApplication.class, args);
+	}
+}
